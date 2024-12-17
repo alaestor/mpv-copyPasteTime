13bfbbd2b8a7085ed16920095c41c74a06550681
Alaestor Weissman <alaestor@null.net>
2024-12-17 14:09:51 -0500

    Simplify by depending on mpv-clipboard

--- README.md
+++ README.md
@@ -1,26 +1,47 @@
 # mpv-copyTimeOrFrame
 
-A fork of [mpv-copyTime](https://github.com/Arieleg/mpv-copyTime) with a few bugfixes from unmerged PRs, expanded to also allow copying MPV's estimated frame number.
+A fork of [mpv-copyTime](https://github.com/Arieleg/mpv-copyTime) with a few bugfixes from unmerged PRs and a couple new features.
 
-Copy the current time (formatted as `HH:MM:SS.sss`) or estimated frame number of the video to clipboard.
+Features:
+
+- Copy the current time (formatted as `HH:MM:SS.sss`) or estimated frame number to clipboard.
+
+# Screenshot
+![ss1](https://user-images.githubusercontent.com/40000640/111867156-02f68a00-8951-11eb-84a8-c78616c68aa3.PNG)
 
 # Installation
 
-Put the script `copyTimeOrFrame.lua` in your scripts folder, usually in:
+## 1. Install dependencies
+
+This script requires [mpv-clipboard](https://github.com/CogentRedTester/mpv-clipboard) be installed.
+
+Windows users may prefer [dyphire's fork](https://github.com/dyphire/mpv-clipboard) as it appears to have some improvements.
+
+## 2. Install the script
+
+Put `copyTimeOrFrame.lua` in your scripts folder, usually in:
 *  Windows: `"%APPDATA%\mpv\scripts"`.
 *  Linux and Mac: `"~/.config/mpv/scripts/"`.
 
-Add your preferred keybinds to your `mpv/inputs.conf` file. e.g.
+## 3. Configure Your Keybinds
+
+There are two ways to do this.
+
+Either...
+
+1. Modify the lines at the bottom of the script by changing `nil` to your desired keybinds. e.g.
+```
+mp.add_key_binding("ctrl+c", "copyFrame", copyFrame)
+mp.add_key_binding("shift+c", "copyTime", copyTime)
+```
+
+or
+
+2. Add your preferred keybinds to your `inputs.conf` file. e.g.
 ```
 ctrl+c script-message-to copyTimeOrFrame copyTime
 shift+c script-message-to copyTimeOrFrame copyFrame
 ```
 
-To work, the script depends on:
-* Windows: `Powershell`.
-* Linux/X11: `xclip`.
-* Linux/Wayland : `wl-clipboard`.
-* MacOS: `pbcopy` (not tested).
+Note: these example keybinds may conflict with your existing keybinds or [MPV's defaults](https://github.com/mpv-player/mpv/blob/master/etc/input.conf).
 
-# Screenshot
-![ss1](https://user-images.githubusercontent.com/40000640/111867156-02f68a00-8951-11eb-84a8-c78616c68aa3.PNG)
--- copyTimeOrFrame.lua
+++ copyTimeOrFrame.lua
@@ -1,75 +1,15 @@
 require 'mp'
-require 'mp.msg'
 
--- Copy the current time of the video to clipboard.
-
-WINDOWS = 2
-UNIX = 3
-
-local function platform_type()
-    local utils = require 'mp.utils'
-    local workdir = utils.to_string(mp.get_property_native("working-directory"))
-    if string.find(workdir, "\\") then
-        return WINDOWS
+local function setClipboard(text)
+   if mp.commandv("script-message", "set-clipboard", text) then
+        mp.osd_message(string.format("Copied to Clipboard: %s", text))
     else
-        return UNIX
-    end
-end
-
-local function command_exists(cmd)
-    local pipe = io.popen("type " .. cmd .. " > /dev/null 2> /dev/null; printf \"$?\"", "r")
-    exists = pipe:read() == "0"
-    pipe:close()
-
-    -- show error if command not exists
-    if not exists and cmd == "pbcopy" then
-        mp.msg.error(cmd .. " package not found! please install it (MacOS-only).")
-    elseif not exists then
-        mp.msg.error(cmd .. " package not found! please install it.")
-    end
-
-    return exists
-end
-
--- reference for io.popen:
--- https://pubs.opengroup.org/onlinepubs/009695399/functions/popen.html
-local function display_servers(cmd)
-    local pipe = io.popen("echo $XDG_SESSION_TYPE", "r")
-    exists = pipe:read() == cmd
-    pipe:close()
-    return exists
-end
-
-local function get_clipboard_cmd()
-    if display_servers("x11") and command_exists("xclip") then
-        return "xclip -silent -in -selection clipboard"
-    elseif display_servers("wayland") and command_exists("wl-copy") then
-        return "wl-copy"
-    elseif command_exists("pbcopy") then
-        return "pbcopy"
-    else
-        mp.msg.error("No supported clipboard command found")
-        return false
+        mp.osd_message(string.format("Failed to copy '%s'.\nAre you missing `mpv-clipboard`?", text))
     end
 end
 
 local function divmod(a, b)
     return a / b, a % b
-end
-
-local function set_clipboard(text)
-    if platform == WINDOWS then
-        mp.commandv("run", "powershell", "set-clipboard", text)
-        return true
-    elseif (platform == UNIX and clipboard_cmd) then
-        local pipe = io.popen(clipboard_cmd, "w")
-        pipe:write(text)
-        pipe:close()
-        return true
-    else
-        mp.msg.error("Set_clipboard error")
-        return false
-    end
 end
 
 local function copyTime()
@@ -79,27 +19,14 @@ local function copyTime()
     local seconds = math.floor(remainder)
     local milliseconds = math.floor((remainder - seconds) * 1000 + 0.5)
     local time = string.format("%02d:%02d:%02d.%03d", math.floor(hours), math.floor(minutes), seconds, milliseconds)
-    if set_clipboard(time) then
-        mp.osd_message(string.format("Copied to Clipboard: %s", time))
-    else
-        mp.osd_message("Failed to copy time to clipboard")
-    end
+    setClipboard(time)
 end
 
 local function copyFrame()
     local frame_number = mp.get_property_number("estimated-frame-number")
     local frame_string = string.format("%d", frame_number)
-    if set_clipboard(frame_string) then
-        mp.osd_message(string.format("Copied to Clipboard: %s", frame_string))
-    else
-        mp.osd_message("Failed to copy frame to clipboard")
-    end
+    setClipboard(frame_string)
 end
 
-platform = platform_type()
-if platform == UNIX then
-    clipboard_cmd = get_clipboard_cmd()
-end
-
-mp.add_key_binding(nil, "copyTime", copyTime)
 mp.add_key_binding(nil, "copyFrame", copyFrame)
+mp.add_key_binding(nil, "copyTime", copyTime)
